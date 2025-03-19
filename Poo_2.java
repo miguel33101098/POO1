efb17854f0ff78bbef78817709448b88da61ed02
miguel10 <aragonriveram@gmail.com>
2025-03-19 18:54:19 -0500

    nuevo resector

---
+++ Poo_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo_2;
+
+/**
+ *
+ * @author arago
+ */
+public class Poo_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
